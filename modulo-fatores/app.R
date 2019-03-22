9abc651eae34902e05f237fff2f198a68395455c
tiagombp <tiagombp@gmail.com>
2019-03-22 17:42:33 -0300

    primeiro teste do app. a função de plotagem está funcionando, mas o app não mostra nada, por algum motivo.

--- modulo-fatores/app.R
+++ modulo-fatores/app.R
@@ -1,3 +1,5 @@
+
+
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
@@ -7,41 +9,71 @@
 #    http://shiny.rstudio.com/
 #
 
+library(tidyverse)
 library(shiny)
+library(shinydashboard)
+library(flexdashboard)
+library(plotly)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Fatores de Variação"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
       ),
-      
-      # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("DPFe_completo")
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  load("fatores.RData")
+  desloc <- 15
+  
+  plota_completo_por_tipo <- function(tipo_divida) {
+    
+    sub_bases <- lista_subs[[tipo_divida]]
+    
+    formata <- function(x){
+      paste0("R$", format(round(x/1e3, 2), big.mark = ".", decimal.mark = ","), " bi")
+    }
+    
+    graf <- plot_ly() %>%
+      add_segments(data = sub_bases[["Juros"]], 
+                   x = ~Periodo, xend = ~Periodo, y = ~y, yend = ~yend, name = "Juros") %>%
+      add_segments(data = sub_bases[["Emissoes"]], text = ~paste(fatores, "<br />",
+                                                                 Periodo, "<br />",
+                                                                 formata(valor)), hoverinfo = "text",
+                   x = ~Periodo, xend = ~Periodo, y = ~y, yend = ~yend, name = "Emissões") %>%
+      add_segments(data = sub_bases[["transf_positiva"]],
+                   x = ~Periodo, xend = ~Periodo, y = ~y, yend = ~yend, name = "Transferências(+)") %>%
+      add_segments(data = sub_bases[["transf_negativa"]], 
+                   x = ~Periodo, xend = ~Periodo, y = ~y, yend = ~yend, name = "Transferências(-)") %>%
+      add_segments(data = sub_bases[["Resgates"]], 
+                   x = ~Periodo+desloc, xend = ~Periodo+desloc, y = ~y, yend = ~yend, name = "Resgates") %>%
+      add_segments(data = sub_bases[["Emissoes"]], 
+                   x = ~Periodo, xend = ~Periodo+desloc, y = ~yend, yend = ~yend,
+                   line = list(dash = "dot", width = 1), name = "") %>%
+      add_segments(data = sub_bases[["Resgates"]], 
+                   x = ~Periodo+desloc, xend = ~prox_periodo, 
+                   y = ~yend, yend = ~yend, line = list(dash = "dot", width = 1), name = "") %>%
+      add_markers(data = sub_bases[["Estoque_ant"]], 
+                  x = ~Periodo, y= ~valor, name = "Estoque")
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    return(graf) 
+  }
+  
+  # plota_completo_por_tipo("DPFe") 
+  
+   output$DPFe_completo <- renderPlotly({
+     plota_completo_por_tipo("DPFe")
    })
 }
 
