16b2bc70675670d44c100a9ccb9800021f59a353
tiagombp <tiagombp@gmail.com>
2019-03-22 11:41:30 -0300

    Processamento inicial ok.

--- modulo-fatores/global.R
+++ modulo-fatores/global.R
@@ -47,7 +47,7 @@ vermelho_claro <- "#ee7576"
 azul_claro     <- "#2c90bf" # "#87b1d4"
 
 
-# importacao dos dados ----------------------------------------------------
+# importacao e preparacao inicial dos dados -------------------------------
 
 tabela <- read_excel("../Anexo_RMD_Dez_18.xlsx", skip = 4, sheet = "2.9")
 
@@ -62,6 +62,8 @@ linhas_de_interesse <- c("Estoque anterior1",
                          "I.1.2 - Resgates",
                          "I.2 - Juros  Apropriados",
                          "II.1 - Transferência de carteira 11")
+
+linha_do_total <- which(tabela$Indicadores == "Total dos Fatores (I + II)")
 
 classificadores <- c("DPMFi", "DPFe")
 
@@ -82,25 +84,51 @@ dados <- tabela %>%
                           Indicadores,
                           NA)) %>%
   fill(Fatores) %>%
-  mutate(TipoDivida = str_extract(Indicadores, paste(classificadores, collapse = "|"))) # (1)
-  filter(Indicadores %in% linhas_de_interesse) %>%
-    
-  left_join(tab_novos_titulos, by = c("Indicadores" = "linhas_de_interesse")) %>%
+  mutate(TipoDivida = str_extract(Indicadores, 
+                                  paste(classificadores, collapse = "|")), # (1)
+         TipoDivida = ifelse(Fatores == "II.1 - Transferência de carteira 11", # (2)
+                             "DPMFi",
+                             TipoDivida)) %>% 
+  filter(row_number() < linha_do_total) %>%
+  filter(!is.na(TipoDivida)) %>%
+  left_join(tab_novos_titulos, by = c("Fatores" = "linhas_de_interesse")) %>%
   select(-Indicadores) %>%
-  select(fatores = lista_fatores, everything()) %>%
-  gather(-fatores, key = mes_ano, value = valor) %>%
-  filter(str_detect(mes_ano, "/")) %>%
-  separate(mes_ano, into = c("Mes", "Ano"), sep = "/") %>%
+  select(fatores = lista_fatores, TipoDivida, everything()) %>%
+  gather(-fatores, -TipoDivida, key = mes_ano, value = valor) %>%
+  mutate(valor = as.numeric(replace_na(valor, 0))) %>%
+  filter(str_detect(mes_ano, "/")) %>% # (3)
+  group_by(fatores, TipoDivida, mes_ano) %>%
+  summarise(valor = sum(valor)) %>%
+  ungroup() %>%
+  separate(mes_ano, into = c("Mes", "Ano"), sep = "/") %>% # (4)
   mutate(Ano = paste0('20', Ano),
          Mes_num = match(Mes, meses),
          Periodo = as.Date(paste0(Ano, "-",
                                   str_pad(Mes_num, 2), "-",
-                                  "01")),
-         valor = as.numeric(valor)) %>%
-  spread(fatores, value = valor) %>%
+                                  "01"))) %>%
+  spread(fatores, value = valor, fill = 0) %>% # (5)
   mutate(transf_positiva = ifelse(Transf > 0, Transf, 0),
          transf_negativa = ifelse(Transf > 0, 0,      Transf)) %>%
   select(-Transf, -Variacao) %>%
-  gather(Emissoes:transf_negativa, key = "fatores", value = "valor")
+  gather(Emissoes:transf_negativa, key = "fatores", value = "valor") %>%
+  spread(TipoDivida, valor) %>%
+  arrange(Periodo) %>%
+  mutate(Total = DPFe + DPMFi)
   
 # (1): Olha que coisa linda... no str_extract, ele procura, para o vetor dado as ocorrências do padrão desejado, e extrai o padrão. Neste caso, eu usei uma lista de padrões que estava definida num vetor ("classificadores"), e usei o paste/collapse para transformar essa lista em uma string do tipo "padrao1|padrao2|padrao3|etc.". Com isso, o que ele faz é detectar se um dos padrões está presente no elemento do vetor pesquisado ("Indicadores"), e retorna justamente o padrão. Perfeito para construir um classificador como o que eu queria aqui.
+
+# (2): Tratamento especial para a transferência de valores, que não está aberta em interna/externa.
+
+# (3): para remover meses que na verdade são anos (os subtotais que eles incluem na planilha)
+
+# (4): tratamento das datas
+
+# (5): espalhando temporariamente para facilitar o cálculo da transf_positiva/negativa
+
+
+# preparacao especifica para o grafico waterfall --------------------------
+
+
+
+
+fatores_ordenados <- c("Estoque_ant", "Juros", "Emissoes", "transf_positiva", "transf_negativa", "Resgates")
