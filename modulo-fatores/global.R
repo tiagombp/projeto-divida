4ed7d079f749b0b0828b75340bba72ed4f2f87ab
tiagombp <tiagombp@gmail.com>
2019-03-22 16:44:41 -0300

    finalizada a preparação inicial. agora gerar os gráficos.

--- modulo-fatores/global.R
+++ modulo-fatores/global.R
@@ -1,22 +1,11 @@
 
-# carga dos temas ---------------------------------------------------------
-
-
+# carga dos pacotes -------------------------------------------------------
 
 library(ckanr)
 library(readxl)
-library(readr)
 library(tidyverse)
 library(stringr)
-library(viridis)
-library(extrafont)
-library(RColorBrewer)
 library(purrr)
-library(plotly)
-library(shiny)
-library(flexdashboard)
-# loadfonts()
-# extrafont::font_import()
 
 
 # definicao do tema -------------------------------------------------------
@@ -128,7 +117,98 @@ dados <- tabela %>%
 
 # preparacao especifica para o grafico waterfall --------------------------
 
-
-
-
 fatores_ordenados <- c("Estoque_ant", "Juros", "Emissoes", "transf_positiva", "transf_negativa", "Resgates")
+
+## essa ideia aqui gerava uma super base, achei melhor separar antes.
+# dados_graf <- dados %>%
+#   filter(fatores %in% fatores_ordenados) %>%
+#   mutate(fatores = factor(fatores, levels = fatores_ordenados, ordered = TRUE)) %>%
+#   arrange(Periodo, fatores) %>%
+#   group_by(Periodo) %>%
+#   mutate_at(vars(c("DPFe", "DPMFi", "Total")), .funs = funs(yend = cumsum(.))) %>%
+#   mutate_at(vars(ends_with("_yend")), .funs = funs(y = lag(.,1))) %>%
+#   rename_at(vars(contains("_yend_")), funs(str_replace(., "_yend_", "_"))) %>%
+#   ungroup() %>%
+#   mutate(prox_periodo = lead(Periodo, length(fatores_ordenados))) %>%
+#   mutate_at(vars(contains("_yend")), 
+#             funs(prox_estoque = lead(., length(fatores_ordenados))))
+
+tipos_divida <- c("DPFe", "DPMFi", "Total")
+
+gera_dados_graf_tipos <- function(tipo_divida) {
+  dadinhos <- dados %>%
+    select(Periodo, fatores, valor = !!sym(tipo_divida)) %>%
+    filter(fatores %in% fatores_ordenados) %>%
+    mutate(fatores = factor(fatores, levels = fatores_ordenados, ordered = TRUE)) %>%
+    arrange(Periodo, fatores) %>%
+    group_by(Periodo) %>%
+    mutate(yend = cumsum(valor),
+           y = lag(yend,1)) %>%
+    ungroup() %>%
+    mutate(prox_periodo = lead(Periodo, length(fatores_ordenados)),
+           prox_estoque = lead(yend, length(fatores_ordenados)))
+  
+  return(dadinhos)
+}
+
+### Purrr map e listas... ver no futuro.
+
+## aqui vou gerar uma lista com os 1df pra cada tipo de dívida
+# dados_graf_tipos <- purrr::map(tipos_divida, gera_dados_graf_tipos)
+# names(dados_graf_tipos) <- tipos_divida
+#
+## para plotar no plotly, agora preciso de um 1df pra cada fator, pra cada tipo de divida
+# gera_dados_graf_tipos_fatores <- function(criterio, tipos) {
+#   return(dados_graf_tipos[[tipos]] %>% filter(fatores == criterio))
+# }
+# 
+# super_lista <- purrr::map(.x = list(criterio = list(fatores_ordenados), 
+#                                     tipos = list(tipos_divida)),
+#                           .f = gera_dados_graf_tipos_fatores)
+
+## desisti, vou fazer na mão
+
+DPFe  <- gera_dados_graf_tipos("DPFe")
+DPMFi <- gera_dados_graf_tipos("DPMFi")
+Total <- gera_dados_graf_tipos("Total")
+
+lista_bases <- list(DPFe, DPMFi, Total)
+
+gera_dados_graf_tipos_fatores <- function(criterios, tipo) {
+  lista <- vector("list", length(criterios))
+  for (i in 1:length(criterios)){
+    lista[[criterios[i]]] <- tipo %>% filter(fatores == criterios[i])
+  }
+  return(lista)
+}
+
+DPFe_sub  <- gera_dados_graf_tipos_fatores(fatores_ordenados, DPFe)
+DPMFi_sub <- gera_dados_graf_tipos_fatores(fatores_ordenados, DPMFi)
+Total_sub <- gera_dados_graf_tipos_fatores(fatores_ordenados, Total)
+# pq gera 12 elementos pra cada um???
+
+# >>>> super-lista das sub-bases para cada tipo de dívida
+lista_subs <- list(DPFe_sub, DPMFi_sub, Total_sub)
+names(lista_subs) <- tipos_divida
+
+
+gera_dados_sem_estoque <- function(dado) {
+  dadinhos_sem_estoque <- dado %>%
+    filter(fatores %in% c("Juros", "Emissoes", "Resgates")) %>%
+    select(Periodo, fatores, valor) %>%
+    spread(fatores, valor) %>%
+    mutate(variacao = Juros + Emissoes + Resgates,
+           sinal = ifelse(variacao > 0, "Positivo", "Negativo"))
+  
+  return(dadinhos_sem_estoque)
+}
+
+# >>>> nem tão super-lista de bases sem estoque
+lista_dados_sem_estoque <- purrr::map(.x = lista_bases,
+                                      .f = gera_dados_sem_estoque)
+names(lista_dados_sem_estoque) <- tipos_divida
+
+
+# salva os objetos para serem usados no aplicativo Shiny ------------------
+
+save(lista_subs, lista_dados_sem_estoque,  file = "fatores.RData")
