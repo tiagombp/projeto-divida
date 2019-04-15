3200c8b5dba33268f9690ee4f936faff629ad325
tiagombp <tiagombp@gmail.com>
2019-04-15 16:17:52 -0300

    processa dados para o formato do plotly. incorpora dados do vazamento.

--- modulo-exec-orc/nec-fin/analise.R
+++ modulo-exec-orc/nec-fin/analise.R
@@ -14,6 +14,9 @@ loadfonts()
 
 load("../exec_orcam.RData")
 
+vazamento <- read_excel("vazamento.xlsx", skip = 9)
+names(vazamento) <- c("Ano", "OutrasDesp")
+
 dados_nec_fin <- dados %>%
   mutate(
     Classificador = case_when(
@@ -28,6 +31,7 @@ dados_nec_fin <- dados %>%
            MovFin = Movimentação.Financeira,
            Ano = Exercicio) %>%
   summarise(Valor = sum(Saldo.Atual)) %>%
+  ungroup() %>%
   filter(Classificador != "Principal Bacen")
 
 # # encargos Bacen
@@ -42,4 +46,32 @@ ggplot(dados_nec_fin %>%
   geom_area() + 
   scale_y_continuous(labels = function(x) {format(x/1e9, big.mark = ".", decimal.mark = ",")})
 
+dados_plotly <- dados_nec_fin %>%
+  mutate(
+    serie = case_when(
+      Classificador == "Encargos Bacen" ~ "Encargos Bacen",
+      Classificador == "Externa" ~ "Externa",
+      Classificador == "Interna" ~ as.character(MovFin))
+  ) %>%
+  group_by(Ano, serie) %>%
+  summarise(Valor = sum(Valor)) %>%
+  ungroup() %>%
+  spread(key = serie, value = Valor) %>%
+  left_join(vazamento, by = "Ano") %>%
+  mutate(Interna       = `Juros e Encargos` + Principal,
+         Vencimentos   = Interna + Externa + `Encargos Bacen`,
+         graf_Juros    = Principal + `Juros e Encargos`,
+         graf_DivEx    = graf_Juros + Externa,
+         graf_EncBCB   = graf_DivEx + `Encargos Bacen`,
+         graf_OutDesp  = graf_EncBCB + OutrasDesp)
+  
+
+cores <- viridis(7)
+
+graf1 <- 
+  plot_ly(dados_plotly, x = ~Ano, type = "scatter", fill = 'tozeroy', y = ~`graf_OutDesp`, 
+        name = "Outras Despesas", mode = "none", stackgroup = 'one', fillcolor = cores[1]) %>%
+  add_trace(y = ~Vencimentos, name = "Vencimentos da DPF", fillcolor = cores[2])
+  
+  
 
